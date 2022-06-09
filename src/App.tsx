2c7adec379439d4c583b126cd158b797fcb88dd7
mateusx111 <mateuszmt@outlook.com>
2022-06-09 08:27:06 -0300

    fished

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import { Header } from './components/Header';
+import { Post } from './components/Post';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
+
+import './global.css';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/mateusx111.png',
+      name: 'Mateus S. Santos',
+      role: 'Web/Mobile Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala Galeraa 👋', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifólio portifólio. É um projeto que fiz durante o evento da rocketseat na NLW Return. O nome do projeto é DoctorCare 🚀', },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-06-07 11:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Maykebrito',
+      role: 'Educator @Roketseat',
+    },
+    content: [
+      { type: 'paragraph', content: 'Faaala Dev 👋', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto na rocketseat para agregar o portifólio de você. É um projeto que fiz para desenvolvermos juntos na NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-06-06 09:30:00')
+  },
+]
+
+export function App() {
+
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          {posts.map(post => {   // se usar o forEach não tem nehum retorno
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )      // map possibilita o retorno
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
+
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,28 @@
+import { PencilSimpleLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
+
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img
+                className={styles.cover}
+                src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+            />
+
+            <div className={styles.profile}>
+            <Avatar src="https://github.com/mateusx111.png" />
+                <strong>Mateus S. Santos</strong>
+                <span>Web Developer / Mobile</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilSimpleLine size={20}/>
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
